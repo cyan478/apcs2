8151d708219512486d58b46b5302c19b81d843f8
cyan478 <cyan2@stuy.edu>
2016-02-09 04:56:19 -0500

    2/9

--- RomanApp.java
+++ RomanApp.java
@@ -26,10 +26,43 @@ public class RomanApp{
 	return ans;
     }
 
+    //sum of Romans in an ArrayList
+    public static Roman sum (ArrayList<Roman> L){
+	int ans  = 0;
+	for (Roman roman : L)
+	    ans += roman.intValue();
+	return new Roman(ans);
+    }
+
+    //L is unchanged, returns the even Roman numbers
+    public static ArrayList<Roman> filterEvens(ArrayList<Roman> L){
+	ArrayList<Roman> evens = new ArrayList<Roman>();
+	for (Roman x : L)
+	    if (x.intValue() % 2 ==0)
+		evens.add(x);
+	return evens;
+    }
+
+    //n is added to each roman numeral in :
+    //if L -> [I,II] mapAdd(1,L)
+    //L becomes [II,III]
+    public static void mapAdd(int n, ArrayList<Roman> L){
+	for (Roman roman : L){
+	    int x = roman.intValue();
+	    x += n;
+	    roman = new Roman(x);
+	}
+    }
+    
+
     public static void main(String [] args){
 	ArrayList<Roman> romans = populate(5,2,100);
 	System.out.println(romans);
 	System.out.println("min : " + min(romans));
+	Number total = sum(romans);
+	System.out.println("Sum: " + total + " int: " + total.intValue());
+	mapAdd(1,romans);
+	System.out.println("mapAdd(1,romans) : " + romans);
     }
 
 
