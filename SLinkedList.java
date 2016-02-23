8d8e3d2572908696c66dfb835b691f40e86311db
cyan478 <cyan2@stuy.edu>
2016-02-23 10:09:49 -0500

    do removeLast()

--- SLinkedList.java
+++ SLinkedList.java
@@ -7,6 +7,10 @@ public class SLinkedList{
 	_head = _tail = null;
 	_size = 0;
     }
+    
+    public int size(){
+	return _size;
+    }
 
     public void addFirst(String value){
 	if (_size ==0)
@@ -16,19 +20,53 @@ public class SLinkedList{
 	_size++;
     }
     
-    public String toString(){
-
+    public void addLast(String value){
+	if (_size == 0) 
+	    addFirst(value);
+	else{
+	    Node last = new Node(value, null); //creates the new node
+	    _tail.setNext(last); //_tail gets ^ that address
+	    _tail = last; //_tail refers to last node
+	    _size++;
+	}
     }
 
-    public static void main(Stirng[] args){
+    public String removeFirst(){
+	if (size() == 0) throw new IllegalStateException();
+	Node first = _head; //create copy/temp of _head
+        if (size() == 1) 
+	    _head = _tail = null; //release it, set to null
+	if (_head != null)
+	    _head = _head.getNext(); //set _head to the next Node
+	_size--;
+	return first.getValue(); //returns the string of the temp
+    }
+
+    public String removeLast(){
+	if (_size <= 1) return removeFirst();
+	
+    }
+
+    //O(n)
+    public String toString(){
+	String ans = "";
+	Node current = _head;
+	for (int i = 0; i < _size; i++){
+	    ans += current.getValue()+" ";
+	    current = current.getNext();
+	}
+    
+    }
+
+    public static void main(String[] args){
 	SLinkedList L = new SLinkedList();
 	System.out.println(L); //""
-	L.addFirst("Sue");
-	System.out.println(L); //Sue
-	L.addFirst("Mary");
-	System.out.println(L); //Mary Sue
-	L.addFirst("Bill");
-	System.out.println(L); //Bill Mary Sue
+	L.addLast("Sue");
+	System.out.println(L); //Sue //flipped
+	L.addLast("Mary");
+	System.out.println(L); //Mary Sue //flipped
+	L.addLast("Bill");
+	System.out.println(L); //Bill Mary Sue //flipped
     }
 
 
