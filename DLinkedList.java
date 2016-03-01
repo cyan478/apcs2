c91b6d9bba6da61abf912d135e1dd06d8fc98826
cyan478 <cyan2@stuy.edu>
2016-03-01 10:01:39 -0500

    3/1

--- DLinkedList.java
+++ DLinkedList.java
@@ -6,7 +6,8 @@ public class DLinkedList{
     public DLinkedList(){
 	_size = 0;
 	_header = new DNode(null,null,null);
-	_trailer = new DNode(null,null,null);
+	_trailer = new DNode(null,_header,null);
+	_header.setNext(_trailer);
     }
 
     public int size(){
@@ -14,8 +15,7 @@ public class DLinkedList{
     }
 
     public boolean isEmpty(){
-	if (_header.getNext() == _trailer) return true;
-	return false;
+        return size() == 0;
     }
 
     public DNode getFirst(){
@@ -28,8 +28,60 @@ public class DLinkedList{
 	return _trailer.getPrevious();
     }
 
+    public DNode getPrevious(DNode current){
+	if (current == _header) 
+	    throw new IllegalArgumentException("cannot move back of header");
+	return current.getPrevious();
+    }
+
+    public DNode getNext(DNode current){
+	if (current == _trailer)
+	    throw new IllegalArgumentException("cannot move forward of trailer");
+	return current.getNext();
+    }
+
+    //precond: b refers to a node in the list
+    //         throws an exception if b is the _header
+    //postcond: inserts node a before node b
+    public void addBefore(DNode b, DNode a){
+        DNode prev = getPrevious(b); //uses getPrevious(x) method
+	prev.setNext(a);
+	a.setPrevious(prev);
+	a.setNext(b);
+	b.setPrevious(a);
+	_size++;
+    }
+
+    public void addLast(DNode x){
+	addBefore(_trailer,x);
+    }
+
+    public void addLast(String value){
+	addLast(new DNode(value,null,null));
+    }
+
+    //postcond: [] if list is empty
+    //          [a ] if one element
+    //          [a, b, c ] 3 elements
+    public String toString(){
+	String ans = "[";
+	DNode current = _header;
+	for (int i = 0; i < size()-1; i++)
+	    ans += current.getNext() + ", ";
+	ans += _trailer.getPrevious().getValue() + " ";
+	ans += "]";
+	return ans;
+    }
 
 
+
+    public static void main(String[] args){
+	DLinkedList L = new DLinkedList();
+	System.out.println("size: " + L.size());
+	L.addLast("Jane");
+	L.addLast("Yay");
+	System.out.println(L);
+    }
 
     
 
