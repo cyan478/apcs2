16c0424a6d6f43c747c99c1eb746549aeec78fc0
Celine Yan <celine@Celines-iMac.home>
2016-03-04 23:53:24 -0500

    did lab 3/4

--- DLinkedList.java
+++ DLinkedList.java
@@ -51,7 +51,8 @@ public class DLinkedList{
 	b.setPrevious(a);
 	_size++;
     }
-
+    
+    //***************************************** add Last
     public void addLast(DNode x){
 	addBefore(_trailer,x);
     }
@@ -60,6 +61,74 @@ public class DLinkedList{
 	addLast(new DNode(value,null,null));
     }
 
+    // inserts node b after node a
+    // throw exception if a is the trailer node
+    public void addAfter(DNode a, DNode b){
+	DNode next = getNext(a); //getNext(x) method
+        b.setNext(next);
+	next.setPrevious(b);
+	b.setPrevious(a);
+	a.setNext(b);	    
+    }
+
+    //***************************************** add First
+    public void addFirst(DNode current){
+	addAfter(_header,current);
+    }
+
+    public void addFirst(String value){
+	addFirst(new DNode(value,null,null));
+    }
+
+    // postcondition: returns the ith String in the list.
+    //                get(0) is the first string
+    //                get(size() - 1) is the last string
+    //                Throw an exception if index < 0 || index >= size()
+    public String get(int i){
+	if (i < 0 || i >= size()) throw new IllegalArgumentException();
+	Node current = getFirst();
+	for (int j = 0; j <= i; j++)
+	    if (j != i)
+		current = current.getNext();
+	    else
+		return current.getValue();
+    }
+
+    //precondition: v != null and v refers to a node in the list
+    //postconditon: removes the node v refers to. v is completely unlinked
+    //              from the list.
+    //              Throws exception if v is header or trailer.
+    public void remove(DNode v){
+	if (v == _header || v == _trailer) throw new IllegalArgumentException();
+	Node afterV = v.getNext();
+	Node beforeV = v.getPrevious();
+	beforeV.setNext(afterV);
+	afterV.setPrevious(beforeV); 
+    }
+
+    // postcondition: removes the first Node and returns its string.
+    //                Throws an exception if the list is empty.   
+    public String removeFirst(){
+	if (isEmpty()) throw new IllegalArgumentException();
+	String first = getFirst.getValue();
+	Node afterFirst = getFirst.getNext();
+	_header.setNext(afterFirst);
+	afterFirst.setPrevious(_header);
+	return first;
+    }
+
+    // postcondition: removes the last node and returns its string.
+    // 		      Throws an exception if the list is empty.
+    public String removeLast(){
+	if (isEmpty()) throw new IllegalArgumentException();
+	String last = getLast.getValue();
+	Node beforeLast = getLast.getPrevious();
+	_trailer.setPrevious(beforeLast);
+        beforeLast.setNext(_trailer);
+	return last;
+    }
+       
+    
     //postcond: [] if list is empty
     //          [a ] if one element
     //          [a, b, c ] 3 elements
