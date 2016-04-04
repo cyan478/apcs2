ba67ee816f473f3e6a83f99c005f2d94f94cc429
cyan478 <cyan2@stuy.edu>
2016-04-04 10:15:11 -0400

    4/4

--- Queue/ArrayQueue.java
+++ Queue/ArrayQueue.java
@@ -1,11 +1,52 @@
-public class ArrayQueue{
+public class ArrayQueue<E>{
 
+    private E[] _queue;
+    private int _front;
+    private int _rear;
+    private int _capacity;
+    private int _size;
 
+    private final static int CAPACITY = 4;
+    
+    public ArrayQueue(int capacity){
+	_queue = (E[]) new Object[capacity];
+	_front = 0;
+	_rear = 0;
+	_size = 0;
+	_capacity = capacity;
+    }
 
+    public ArrayQueue(){
+        this(CAPACITY);
+    }
+    
+    public void enqueue(E element) throws FullQueueException{
+	if (size() == _capacity) throw new FullQueueException("Full");
+	_queue[_rear] == element;
+	_rear = (1+_rear)%_capacity;
+	size++;
+    }
 
+    public E dequeue() throws EmptyQueueException{
+	E ans = front();
+	queue[_front] = null; //release it
+	_front = (1+_rear)%_capacity; //readjust _front
+	_size++;
+	return ans;
+    }
 
+    public int size(){
+	return _size;
+    }
 
+    public boolean isEmpty();{
+	return size() == 0;
+    }
 
+    public E front() throws EmptyQueueException{
+	if (isEmpty()) throw new EmptyQueueException("Empty");
+	return _queue[_front];
+    }
 
 
 } //end
--- Queue/Queue.java
+++ Queue/Queue.java
@@ -2,12 +2,12 @@ public interface Queue<E>{
 
     public void enqueue(E element);
 
-    public E dequeue();
+    public E dequeue() throws EmptyQueueException;
 
     public int size();
 
     public boolean isEmpty();
 
-    public E front();
+    public E front() throws EmptyQueueException;
 
 } //end
