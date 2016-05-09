fcbf1f0a151cf77c239de076010d49c159895df9
cyan478 <cyan2@stuy.edu>
2016-05-09 10:15:14 -0400

    finish for hw

--- Trees/BST.java
+++ Trees/BST.java
@@ -1,4 +1,4 @@
-public class BST<E extends Comparable>{
+public class BST<E implements Comparable>{
 
     private TreeNode<E> _root;
     private int _size;
@@ -12,12 +12,54 @@ public class BST<E extends Comparable>{
 	insertNode(new TreeNode<E>(x));
     }
 
-    private void insertNode(TreeNode<E> x){
-	if (_root == null){
-	    _root = x;
-	    return;
+    private void insertNode(TreeNode<E> n){
+        E val = n.getValue();
+	if (_root == null) _root = n;
+	else{
+	    TreeNode<E> curr = _root;
+	    TreeNode<E> prev = curr;
+	    while (curr!=null){
+		prev = curr;
+		if(val.compareTo(curr.getValue()) >= 0)
+		    curr = curr.getRight();
+		else
+		    curr = curr.getLeft();
+	    }
+	    if (val.compareTo(prev.getValue()) >= 0)
+		prev.setRight(n);
+	    else
+		prev.setLeft(n);
 	}
-	if (
+	size++;
+    }
+
+    public void insert(E x){
+	_root = insertNode(_root, new TreeNode<E>(x));
+	_size++;
+    }
+
+    private TreeNode<E> insertNode(TreeNode<E> rt, TreeNode<E> n){
+
+    }
+
+    public static<E> int height(TreeNode<E> rt){
+	if (rt==null || isLeaf(rt)) return 0;
+	int leftCount = height(rt.getLeft());
+	int rightCount = height(rt.getRight());
+	return 1 + Math.max(leftCount,rightCount);
+    }
+
+    public boolean isBalanced(){
+	return isBalanced(_root);
+    }
+    
+    private boolean isBalanced(TreeNode<E> rt){
+	if (rt == null) return true;
+	int h1 = (height(rt.getLeft()));
+	int h2 = (height(rt.getRight()));
+	if (Math.abs(h1-h2) > 1)
+	    return false;
+	return isBalanced(rt.getLeft()) && isBalanced(rt.getRight());
     }
 
     public int size(){
