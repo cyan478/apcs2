6ac806d7e1bc1b265b31321fd1f5a5c0e5d293a6
Celine Yan <celine@Celines-iMac.home>
2016-05-21 22:05:22 -0400

    FOR REFgit add scnr.java try it out c':

---
+++ scnr.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class scnr {
+
+    public static void main(String[] args) {
+
+        //System.in means scanner reads from terminal;
+        //Scanner can also read from files and shit
+        Scanner io = new Scanner(System.in);
+
+        System.out.print("Type in noun: ");
+        String x = io.next();
+        //the above 2 lines gets whatever the user types into terminal,
+        //and sets string x equal to what they write
+        System.out.println("Don't you just love " + x + " flavored jizz?");
+
+        //the above is for strings, how can we get numbers they type in?
+        System.out.print("Type in number: ");
+        int datnumber = io.nextInt();
+        System.out.println("Your IQ is " + datnumber + ".");
+
+    }
+}
